b7c5afa6324bec2ebc3c240abff85010b32221a0
Mark S. Lewis <mark_lewis@uk.ibm.com>
2021-01-20 15:29:09 +0000

    FABGW-15: Convert Node scenario test to TypeScript
    First-pass conversion with some follow-on work required to avoid disabling noImplicitThis checking.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- node/src/index.ts
+++ node/src/index.ts
@@ -4,9 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+export { Contract } from './contract';
+export { connect, ConnectOptions, Gateway } from './gateway';
+export { Identity } from './identity/identity';
 export { Signer } from './identity/signer';
 export * as signers from './identity/signers';
-export { Identity } from './identity/identity';
-export { connect, ConnectOptions, Gateway } from './gateway';
 export { Network } from './network';
-export { Contract } from './contract';
+export { Proposal } from './proposal';
+export { ProposalOptions } from './proposalbuilder';
+export { Transaction } from './transaction';
+
