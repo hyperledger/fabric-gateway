3424ee7a6f108c29b00f8999c7c980dccacb27b0
Andrew Coleman <andrew_coleman@uk.ibm.com>
2021-03-30 14:33:46 +0100

    Gateway Java SDK sample (#78)
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

---
+++ samples/java/src/test/java/com/example/SampleTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.example;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testSample() throws Exception {
+        Sample.main(null);
+    }
+}
