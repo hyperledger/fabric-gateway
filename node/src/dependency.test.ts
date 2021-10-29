742ebec0c5984644187b6f42d12c23b208b4f74d
Mark S. Lewis <mark_lewis@uk.ibm.com>
2021-10-29 10:48:54 +0100

    Dynamic load of optional pkcs11js dependency
    Prevents this dependency being loaded unless an HSM signer is actually created, avoiding runtime issues on systems that do not have the optional pkcs11js dependency available.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

---
+++ node/src/dependency.test.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2021 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { generateKeyPairSync } from 'crypto';
+
+function isLoaded(moduleName: string): boolean {
+    const moduleFile = require.resolve(moduleName);
+    return !!Object.values(require.cache).find(m => m.filename === moduleFile);
+}
+
+describe('optional pkcs11js dependency', () => {
+    it('not loaded when accessing private key signer', async () => {
+        jest.resetModules();
+        expect(isLoaded('pkcs11js')).toBe(false);
+
+        const { privateKey } = generateKeyPairSync('ec', { namedCurve: 'P-256' });
+        const { signers } = await import('.');
+        signers.newPrivateKeySigner(privateKey);
+
+        expect(isLoaded('pkcs11js')).toBe(false);
+    });
+});
