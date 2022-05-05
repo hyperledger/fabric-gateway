11f1401f22619d7cdd1ac7ebde65492842e4562a
sapthasurendran <saptha.surendran@ibm.com>
2022-05-05 19:11:21 +0530

    java refactor
    Signed-off-by: sapthasurendran <saptha.surendran@ibm.com>

--- java/src/test/java/scenario/CheckpointEventListener.java
+++ java/src/test/java/scenario/CheckpointEventListener.java
@@ -5,11 +5,12 @@ import org.hyperledger.fabric.client.CloseableIterator;
 import java.util.function.Consumer;
 
 public final class CheckpointEventListener<T> implements Events<T> {
-  EventListener<T> eventListener;
-  Consumer<T> checkpoint;
+  private final EventListener<T> eventListener;
+  private final Consumer<T> checkpoint;
+
   CheckpointEventListener(CloseableIterator<T> iterator, Consumer<T> checkpoint) {
     eventListener = new EventListener<T>(iterator);
-    checkpoint = checkpoint;
+    this.checkpoint = checkpoint;
   }
   @Override
   public T next() {
