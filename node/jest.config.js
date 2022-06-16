3b340884447004591c1a1dd0834b7e8b7014d7a5
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2022-06-16 14:34:19 +0100

    Exclude unit test files from TypeScript build (#454)
    Previously there were being filtered from the build output at the npm packaging stage.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- node/jest.config.js
+++ node/jest.config.js
@@ -2,16 +2,16 @@ module.exports = {
     "roots": [
         "<rootDir>/src"
     ],
-    "preset": "ts-jest",
-    "testEnvironment": "node",
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-        "**/*.[jt]s?(x)",
-        "!**/*.d.ts",
+    'preset': 'ts-jest',
+    'testEnvironment': 'node',
+    'collectCoverage': true,
+    'collectCoverageFrom': [
+        '**/*.[jt]s?(x)',
+        '!**/*.d.ts',
     ],
-    "coverageProvider": "v8",
-    "testMatch": [
-        "**/?(*.)+(spec|test).[jt]s?(x)"
+    'coverageProvider': 'v8',
+    'testMatch': [
+        '**/?(*.)+(spec|test).[jt]s?(x)'
     ],
-    "maxWorkers": 1, // Workaround for Jest BigInt serialization bug: https://github.com/facebook/jest/issues/11617
+    'maxWorkers': 1, // Workaround for Jest BigInt serialization bug: https://github.com/facebook/jest/issues/11617
 }
