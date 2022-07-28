6c09dfc95f0c819595074c313b42f76ce67f208f
Mark S. Lewis <mark_lewis@uk.ibm.com>
2022-07-28 13:08:43 +0100

    Simplify code to generate canonical ECDSA signatures
    Add additional testing for malleability and benchmarking.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- pkg/identity/sign_test.go
+++ pkg/identity/sign_test.go
@@ -8,7 +8,11 @@ package identity
 
 import (
 	"crypto"
+	"crypto/ecdsa"
+	"encoding/asn1"
 	"fmt"
+	"math/big"
+	"math/rand"
 	"testing"
 
 	"github.com/hyperledger/fabric-gateway/pkg/internal/test"
@@ -18,6 +22,9 @@ import (
 func TestSigner(t *testing.T) {
 	privateKey, err := test.NewECDSAPrivateKey()
 	require.NoError(t, err)
+
+	digest := make([]byte, 256/8)
+	rand.Read(digest)
 
 	t.Run("Create signer with unsupported private key type fails", func(t *testing.T) {
 		var privateKey crypto.PrivateKey
@@ -32,9 +39,43 @@ func TestSigner(t *testing.T) {
 		sign, err := NewPrivateKeySign(privateKey)
 		require.NoError(t, err)
 
-		signature, err := sign([]byte("digest"))
+		signature, err := sign(digest)
+		require.NoError(t, err, "sign")
+
+		isValid := ecdsa.VerifyASN1(&privateKey.PublicKey, digest, signature)
+		require.True(t, isValid, "valid signature")
+	})
+
+	t.Run("ECDSA signatures are canonical", func(t *testing.T) {
+		sign, err := NewPrivateKeySign(privateKey)
 		require.NoError(t, err)
 
-		require.NotEmpty(t, signature)
+		halfOrder := new(big.Int).Rsh(privateKey.Params().N, 1)
+
+		for i := 0; i < 10; i++ {
+			signature, err := sign(digest)
+			require.NoError(t, err, "sign")
+
+			signatureRS := &ecdsaSignature{}
+			_, err = asn1.Unmarshal(signature, signatureRS)
+			require.NoError(t, err, "asn1.Unmarshal")
+
+			require.LessOrEqual(t, signatureRS.S.Cmp(halfOrder), 0, "malleable: S = %v, halfOrder = %v", signatureRS.S, halfOrder)
+		}
 	})
+}
+
+func BenchmarkECDSA(b *testing.B) {
+	privateKey, err := test.NewECDSAPrivateKey()
+	require.NoError(b, err)
+
+	sign, err := NewPrivateKeySign(privateKey)
+	require.NoError(b, err)
+
+	digest := make([]byte, 256/8)
+	rand.Read(digest)
+
+	for i := 0; i < b.N; i++ {
+		sign(digest)
+	}
 }
