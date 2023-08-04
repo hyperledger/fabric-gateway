1283b9c118ee31ecffb9092b33dc215a4cf5e06d
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2023-08-04 14:29:52 +0100

    Use Java 17 for publishing (#617)
    Java 8 was used for safety, since the intention is to maintain compatibility with Java 8 by compiling compatible bytecode and only using Java 8 standard libraries. This caused a Javadoc failure when linking to Javadoc built with later versions of Java, since they produce a different format of package-list / element-list file. This problem surfaced when gRPC-Java started to publish builds using Java 11 instead of Java 8, and caused Javadoc warnings linking to their published Javadoc.
    
    Use Java 17 for publishing, relying on the maven.compiler.release (javac --release) option to ensure that compiled output links to the Java 8 standard libraries. To allow this, no longer enforce exactly Java 8 in the Maven release profile.
    
    Fix some broken links in Javadoc.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- java/src/main/java/org/hyperledger/fabric/client/package-info.java
+++ java/src/main/java/org/hyperledger/fabric/client/package-info.java
@@ -9,9 +9,11 @@
  * interact with a Fabric blockchain network. It provides a simple API to
  * submit transactions to a ledger or query the contents of a ledger with minimal code.
  * The Gateway SDK implements the Fabric programming model as described in the
- * <a href="https://hyperledger-fabric.readthedocs.io/en/latest/developapps/developing_applications.html">Developing Applications</a>
- * chapter of the Fabric documentation.</p>
+ * <a href="https://hyperledger-fabric.readthedocs.io/en/latest/write_first_app.html">Running a Fabric Application</a>
+ * tutorial of the Fabric documentation.</p>
  *
- * @see <a href="https://hyperledger-fabric.readthedocs.io/en/latest/developapps/developing_applications.html">Developing Fabric Applications</a>
+ * <p>The entry point into this package is {@link org.hyperledger.fabric.client.Gateway Gateway}.</p>
+ *
+ * @see <a href="https://hyperledger-fabric.readthedocs.io/en/latest/write_first_app.html">Running a Fabric Application</a>
  */
 package org.hyperledger.fabric.client;
