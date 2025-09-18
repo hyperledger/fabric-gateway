b984d6ebb0cb62442b851df8280b2b918a4e1c81
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2025-09-18 11:34:21 +0100

    Group dependabot updates (#927)
    Minimise use of build resources by grouping dependabot updates for each
    ecosystem into a single pull request. Apply concurrency rules to
    minimise the number of builds triggered when multiple pull requests are
    merged in close succession.
    
    Also publish unstable npm packages to GitHub packages instead of the
    public NPM registry to reduce the burden of dependency updates.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- .github/scripts/npm_publish.sh
+++ .github/scripts/npm_publish.sh
@@ -6,11 +6,8 @@ TAG="${1:?}"
 
 if [[ ${TAG} != latest ]]; then
     PACKAGE_VERSION=$(jq --raw-output .version package.json)
-    TODAY=$(date -u '+%Y%m%d')
-    TARGET_VERSION="${PACKAGE_VERSION}-dev.${TODAY}."
-    INCREMENT=$(npm view --json | jq --raw-output '.versions[]' | awk -F . "/^${TARGET_VERSION}/"'{ lastVersion=$NF } \
-        END { sub(/".*/, "", lastVersion); print (lastVersion=="" ? "1" : lastVersion+1) }')
-    PUBLISH_VERSION="${TARGET_VERSION}${INCREMENT}"
+    NOW=$(date -u '+%Y%m%d.%H%M%S')
+    PUBLISH_VERSION="${PACKAGE_VERSION}-dev.${NOW}"
     npm --allow-same-version --no-git-tag-version version "${PUBLISH_VERSION}"
 fi
 
